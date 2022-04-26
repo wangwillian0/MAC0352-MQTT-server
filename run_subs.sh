374292955941d4ec83d02216df0497da6e10b958
Willian Wang <git@willian.wang>
2022-04-26 00:20:49 -0300

    Melhora scripts de teste

--- run_subs.sh
+++
@@ -1,9 +0,0 @@
-clients=$1
-topics=$2
-
-for ((i=0;i<clients;i++)); do
-	topic_id=$((i%topics))
-	mosquitto_sub -V 311 -h localhost -t "<$topic_id> topico_relativamente_grande <$topic_id>" > /dev/null & 
-done
-
-echo "use 'killall mosquitto_sub' após o uso"
---
+++ run_tests.sh
@@ -0,0 +1,42 @@
+if [[ $# -ne 4 ]] ; then
+	echo "Use ./run_tests.sh <clientes> <topicos> <tempo de monitoramento> <quantas vezes testar>"
+	exit 0
+fi
+
+clients=$1
+topics=$2
+duration=$3
+samples=$4
+
+function monitor_stats {
+	calls=$((duration/2))
+	for ((i=0;i<calls;i++)); do
+		docker stats --no-stream --format "{{.CPUPerc}},{{.NetIO}}" >> $1
+	done
+}
+
+function run_subs {
+	for ((i=0;i<clients;i++)); do
+		topic_id=$((i%topics))
+		mosquitto_sub -V 311 -h localhost -t "<$topic_id> topico_relativamente_grande <$topic_id>" > /dev/null & 
+	done
+}
+
+function run_pubs {
+	for ((i=0;i<clients;i++)); do
+		topic_id=$((i%topics))
+		mosquitto_pub -V 311 -h localhost -t "<$topic_id> topico_relativamente_grande <$topic_id>" -m "<$topic_id> ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*() mensagem um pouco mais comprido para o topico com topic_id = <$topic_id>." &  
+	done
+}
+
+
+for ((i=0;i<samples;i++)); do
+	filename="teste_$i"
+	monitor_stats $filename &
+	run_subs &
+	run_pubs &
+	killall mosquitto_pub
+	killall mosquitto_sub
+done
+
+	
